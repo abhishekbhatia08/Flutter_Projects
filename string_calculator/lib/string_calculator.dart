97d6653509617748729a02090a40b73ccbed63cb
Abhishek Bhatia <abhibhatia2000@gmail.com>
2025-02-12 02:48:15 +0530

    Added delimiters handling

--- string_calculator/lib/string_calculator.dart
+++ string_calculator/lib/string_calculator.dart
@@ -5,6 +5,16 @@ class StringCalculator {
       return 0;
     }
 
+    final List<String> delimiters = [",", "\n"];
+
+    // Handling custom delimiters
+    if (numbers.startsWith("//")) {
+      final parts = numbers.split('\n');
+      final customDelimiters = parts.first.substring(2);
+      delimiters.add(customDelimiters);
+      numbers = parts.sublist(1).join("\n");
+    }
+
     return 0;
   }
 }
